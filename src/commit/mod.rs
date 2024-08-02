30c99468a4b8927a85652f2d2b0d6654d5c2943a
liting <luz.liting@gmail.com>
2024-08-02 23:13:06 +0800

    chore(commit): extract error handle

--- src/commit/mod.rs
+++ src/commit/mod.rs
@@ -5,6 +5,15 @@ use console::style;
 use inquire::{required, InquireError, Select, Text};
 use std::process::{exit, Command};
 
+fn handle_inquire_error(err: InquireError) {
+    match err {
+        InquireError::OperationCanceled | InquireError::OperationInterrupted => exit(1),
+        _ => {
+            println!("There was an error, please try again")
+        }
+    }
+}
+
 pub fn handle_commit() {
     let mut commit_message = types::CommitMessage::new();
     let config = get_config();
@@ -15,54 +24,30 @@ pub fn handle_commit() {
         .map(|item| item.key.to_string())
         .collect();
     let r#type = Select::new("Select the type of change that you're committing: ", types)
-        .with_help_message("↑↓ to move, enter to select, type to filter (control + D to exit)")
+        .with_help_message("↑↓ to move, enter to select, type to filter (Ctrl-C to exit)")
         .prompt();
     match r#type {
         Ok(r#type) => {
             commit_message.set_type(r#type.to_string());
         }
-        Err(err) => match err {
-            InquireError::OperationCanceled => {
-                println!("{}", style("Commit canceled").yellow());
-                exit(1)
-            }
-            _ => {
-                println!("There was an error, please try again")
-            }
-        },
+        Err(err) => handle_inquire_error(err),
     }
 
     let scope = Text::new("What is the scope of this change: ")
-        .with_help_message("e.g. component or file name (control + D to exit)")
+        .with_help_message("e.g. component or file name (Ctrl-C to exit)")
         .prompt();
     match scope {
         Ok(scope) => commit_message.set_scope(scope),
-        Err(err) => match err {
-            InquireError::OperationCanceled => {
-                println!("{}", style("Commit canceled").yellow());
-                exit(1)
-            }
-            _ => {
-                println!("There was an error, please try again")
-            }
-        },
+        Err(err) => handle_inquire_error(err),
     }
 
     let subject = Text::new("Write a short, imperative tense description of the change: ")
         .with_validator(required!("Subject cannot be empty."))
-        .with_help_message("control + D to exit")
+        .with_help_message("Ctrl-C to exit")
         .prompt();
     match subject {
         Ok(subject) => commit_message.set_subject(subject),
-        Err(err) => match err {
-            InquireError::OperationCanceled => {
-                println!("{}", style("Commit canceled").yellow());
-                exit(1)
-            }
-            _ => {
-                println!("There was an error, please try again")
-            }
-        },
+        Err(err) => handle_inquire_error(err),
     }
 
     let output = Command::new("git")
