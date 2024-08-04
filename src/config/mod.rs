b57845b091c288e91e3b72bd670a380ac4898a8e
liting <luz.liting@gmail.com>
2024-08-04 14:26:17 +0800

    feat(config): support load global & local config files

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,7 +1,10 @@
-use config::{Config, File};
-use serde::{Deserialize, Serialize};
+use std::env::current_dir;
 use std::fmt;
 use std::fmt::Formatter;
+use std::path::{Path, PathBuf};
+
+use config::{Config, File};
+use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
 pub struct LitingConfigType {
@@ -20,8 +23,29 @@ pub struct LitingConfig {
 }
 
 pub fn get_config() -> LitingConfig {
+    let mut global_config_path = PathBuf::from(dirs::home_dir().unwrap());
+    global_config_path.push("liting");
+    let mut local_config_path = PathBuf::from(current_dir().unwrap());
+    local_config_path.push("liting");
+
     let config = Config::builder()
-        .add_source(File::with_name("src/config/liting.default.toml"))
+        .add_source(File::with_name("src/config/liting.default"))
+        .add_source(
+            File::from(Path::new(
+                global_config_path
+                    .to_str()
+                    .expect("Load global config file path failed."),
+            ))
+            .required(false),
+        )
+        .add_source(
+            File::from(Path::new(
+                local_config_path
+                    .to_str()
+                    .expect("Load local config file path failed."),
+            ))
+            .required(false),
+        )
         .build()
         .expect("Get config failed");
 
